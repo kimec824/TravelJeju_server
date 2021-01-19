2ccacb99adf6a32d5a1726e30c85532da1b4acb2
kimec824 <kimec@postech.ac.kr>
2021-01-19 10:49:26 +0000

    region post, delete 추가

--- app.js
+++ app.js
@@ -194,3 +194,81 @@ app.delete('/surveyresult',function(req,res,next){
         } 
     });
 });
+
+
+app.post('/region',function(req,res,next){
+    collection_name = 'region'  
+    console.log("enter to post region");
+    mongoClient.connect('mongodb://localhost/', function(error, client){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("connected: " + db_name);
+            mydb = client.db(db_name);
+
+            collection = mydb.collection(collection_name);
+            collection.find({}).toArray(function(err, results){
+                res.status(200).json({'myCollection' : results});
+                console.log(results);
+              });
+            
+            //collection.insert({"name":req.body.name, "phone":req.body.phone, "email":req.body.email});
+            if(Object.keys(req.body).length !== 0)
+                collection.insert(req.body);
+
+            ////////////// For DEBUG ////////////////////
+            var cursor = mydb.collection(collection_name).find();
+            cursor.each(function (err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    if (doc != null) {
+                        console.log(doc);
+                    }
+                    else {
+                        console.log("END");
+                    }
+                }
+            });
+          /////////////////////////////////////////////  
+        } 
+    });
+});
+
+
+app.delete('/region',function(req,res,next){
+    collection_name = 'region'  
+    console.log("enter to delete region");
+    mongoClient.connect('mongodb://localhost/', function(error, client){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("connected: " + db_name);
+            mydb = client.db(db_name);
+
+            collection = mydb.collection(collection_name);
+            collection.find({}).toArray(function(err, results){
+                res.status(200).json({'myCollection' : results});
+              });
+            
+            collection.deleteMany({});
+            ////////////// For DEBUG ////////////////////
+            var cursor = mydb.collection(collection_name).find();
+            cursor.each(function (err, doc) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    if (doc != null) {
+                        console.log(doc);
+                    }
+                    else {
+                        console.log("END");
+                    }
+                }
+            });
+          /////////////////////////////////////////////  
+        } 
+    });
+});
